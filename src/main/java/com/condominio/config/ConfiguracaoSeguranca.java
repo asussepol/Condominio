ee63d8f64eab9080a1062812d353c7f2950c97f4
asussepol <asussepol@gmail.com>
2017-09-25 17:15:49 -0300

    alteracao no cadastro unidades

--- src/main/java/com/condominio/config/ConfiguracaoSeguranca.java
+++ src/main/java/com/condominio/config/ConfiguracaoSeguranca.java
@@ -3,10 +3,12 @@ package com.condominio.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
 
 	@Override
